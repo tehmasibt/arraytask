10c5e9930ea6f9539337676e7bb564eb2f2150df
thmasibtagiyev0@gmail.com <tahmasibmt@code.edu.az>
2024-02-21 09:36:36 +0400

    Add project files.

---
+++ arraytask/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+#region task-1
+//int skafen = 4;
+//int skafuz = 2;
+//int skafhun = 6;
+//int qapien  = 1;
+//int qapihun = 11;
+//if (skafen < qapien && skafhun < qapihun)
+//{
+//    Console.WriteLine("skaf qapidan kecir");
+//}
+//else if (skafuz < qapihun && skafen<qapien)
+//{
+//    Console.WriteLine("skaf qapidan kecir");
+//}
+//else if (skafuz < qapien && skafhun < qapihun)
+//{
+//    Console.WriteLine("skaf qapidan kecir");
+//}
+//else
+//{
+//    Console.WriteLine("skaf qapidan kecmir");
+//}
+#endregion
+#region task-2
+//double a = 64;
+//while (a > 1)
+//{
+//    a /= 2;
+//}
+//if (a == 1)
+//{
+//    Console.WriteLine("quvvetidir");
+//}
+//else
+//{
+//    Console.WriteLine("quvvetid deyil");
+//}
+#endregion
+#region task-3
+//int b = 29728543;
+//int count = 0;
+//while (b>=1)
+//{
+//    b /= 10;
+//    count++;
+//}
+//Console.WriteLine(count);
+#endregion
